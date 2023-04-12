e38ea04d2552ff07536bd340ad48d740405807a1
Maxim Shabunin <120470036+mshabunin97@users.noreply.github.com>
2023-04-12 17:00:21 +0300

    Add files via upload

---
+++ js/style.js
@@ -0,0 +1,11 @@
+new Swiper('.slider', {
+    // direction: 'vertical',
+    speed: 2400,
+    mousewheel: {
+        enabled: true,
+        sensitivity: 2.6
+    },
+    spaceBetween: 18,
+    parallax: true,
+    freeMode: true
+})
